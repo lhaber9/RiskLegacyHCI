8c1d5e528194e5e7858ab2acbeca1304a8180d6f
Lucas Haber <lhaber9@gmail.com>
2016-02-24 16:08:17 -0500

    setting some stuff up

--- application.rb
+++ application.rb
@@ -21,11 +21,7 @@ get "/" do
   erb :root
 end
 
-get "/about" do
-  erb :about
-end
-
-get "/assignments/:number" do
-  fileName = "assignment" + params[:number].to_s
+get "/:fileName" do 
+  fileName = params[:fileName].to_s
   erb fileName.to_sym
 end
