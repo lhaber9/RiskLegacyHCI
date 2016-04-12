15429f83fc64bafc80dab2218a4b822c21db4533
Lucas Haber <lhaber9@gmail.com>
2016-04-12 10:11:01 -0400

    add auto complete on setup screen

--- public/prototype/js/setupController.js
+++ public/prototype/js/setupController.js
@@ -13,12 +13,69 @@
         $scope.chooseFaction = chooseFaction;
 
         $scope.$rootScope = $rootScope;
-        
+
         $scope.availableFactions = ["Die Mechaniker", 
                                     "Enclave of the Bear", 
                                     "Imperial Balkania", 
                                     "Khan Industries", 
                                     "The Saharan Republic"];
+
+        $scope.availableTerritories = [ "Afghanistan",
+                                        "Alaska", 
+                                        "Alberta ",
+                                        "Argentina",
+                                        "Brazil",
+                                        "Central Africa",
+                                        "Central America",
+                                        "China",
+                                        "East Africa",
+                                        "Eastern Australia",
+                                        "Eastern United States",
+                                        "Egypt",
+                                        "Great Britain",
+                                        "Greenland",
+                                        "Iceland",
+                                        "India",
+                                        "Indonesia",
+                                        "Irkutsk",
+                                        "Japan",
+                                        "Kamchatka",
+                                        "Madagascar",
+                                        "Middle East",
+                                        "Mongolia",
+                                        "New Guinea",
+                                        "North Africa",
+                                        "Northern Europe",
+                                        "Northwest Territory",
+                                        "Ontario",
+                                        "Peru",
+                                        "Quebec",
+                                        "Scandinavia",
+                                        "Siam",
+                                        "Siberia",
+                                        "South Africa",
+                                        "Southern Europe",
+                                        "Ukraine",
+                                        "Ural",
+                                        "Venezuela",
+                                        "Western Australia",
+                                        "Western Europe",
+                                        "Western United States",
+                                        "Yakutsk" ];
+
+
+        function setupAutocomplete() {
+            $( ".territoryInput" ).autocomplete({
+              source: $scope.availableTerritories,
+              messages: {
+                    noResults: '',
+                    results: function() {}
+                },
+                select: function (event, ui) {
+                     $scope.newForm.startingTerritory = ui.item.value;                       
+                 }  
+            });
+        }
 
         $rootScope.gameStarted = false;
 
@@ -27,18 +84,10 @@
         }
     	
         $scope.clearFormInput();
+        setupAutocomplete();
 
         function chooseFaction(faction) {
             $scope.newForm.faction = faction;
-
-            for (factionIdx in $scope.availableFactions) {
-                var aFaction = $scope.availableFactions[factionIdx];
-                if (aFaction == faction) {
-                    // console.log("here");
-                    $scope.availableFactions.splice(factionIdx, 1);
-                    return;
-                }
-            }
         }
 
         function newPlayerIsEmpty() {
@@ -62,6 +111,8 @@
                 "faction": "", 
                 "startingTerritory": ""
             }
+
+            $( ".territoryInput" ).val("");
         }
 
         function addPlayer() {
@@ -69,17 +120,38 @@
                 return;
             }
             $rootScope.players.push($scope.newForm);
+            
+            for (territoryIdx in $scope.availableTerritories) {
+                var territory = $scope.availableTerritories[territoryIdx];
+                if (territory == $scope.newForm.startingTerritory) {
+                    $scope.availableTerritories.splice(territoryIdx, 1);
+                    break;
+                }
+            }
+
+            for (factionIdx in $scope.availableFactions) {
+                var aFaction = $scope.availableFactions[factionIdx];
+                if (aFaction == $scope.newForm.faction) {
+                    $scope.availableFactions.splice(factionIdx, 1);
+                    break;
+                }
+            }
+
+            // setupAutocomplete(); 
             $scope.clearFormInput();
         }
 
         function removePlayer(player) {
             $scope.availableFactions.push(player.faction);
+            $scope.availableTerritories.push(player.startingTerritory);
 
             for (playerIdx in $rootScope.players) {
                 if ($rootScope.players[playerIdx].id == player.id) {
                     $rootScope.players.splice(playerIdx, 1);
                 }
             }
+
+            // setupAutocomplete();
         }
 
         function tryStart() {
